6585577d28c8d53ea6b4392d993e213d3d4fdfd6
Brian W <brian.weinfeld+cuny@gmail.com>
2018-02-23 16:49:05 -0500

    cleaned up, no more loops

--- nhlstats.R
+++ nhlstats.R
@@ -3,31 +3,39 @@ library(magrittr)
 library(mysportsfeedsR)
 authenticate_v1_x('njpsy','asdfasdf')
 
-game.schedule <- data.frame(msf_get_results(league='nhl', season='2015-2016-regular', feed='full_game_schedule')[["api_json"]][["fullgameschedule"]][["gameentry"]]) %>%
+
+# get NHL schedule --------------------------------------------------------
+game.schedule <- msf_get_results(league='nhl', 
+                                 season='2015-2016-regular', 
+                                 feed='full_game_schedule')[["api_json"]][["fullgameschedule"]][["gameentry"]] %>%
+  data.frame() %>%
   subset(select=c(6, 12, 16)) %>%
-  setNames(c('date', 'away', 'home'))
+  setNames(c('date', 'away', 'home')) %>%
+  mutate(request = paste(str_replace_all(.$date, '-', ''), .$away, .$home, sep='-')) %T>%
+  write.csv('C:\\Users\\Brian\\Desktop\\GradClasses\\Spring18\\nhlstats\\schedule.csv')
+# get NHL schedule --------------------------------------------------------
 
-game.schedule$request <- paste(str_replace_all(game.schedule$date, '-',''), game.schedule$away, game.schedule$home, sep='-')
 
-game.schedule <- game.schedule[1:100, ]
+# get individual game data ------------------------------------------------
+#add home and away teams?????
+game.schedule <- read.csv('C:\\Users\\Brian\\Desktop\\GradClasses\\Spring18\\nhlstats\\schedule.csv')
 
-#data <- msf_get_results(version='1.2',league='nhl',season='2015-2016-regular',feed='game_playbyplay', params=list(gameid=game.schedule$request[1], playtype='faceoff,goal,penalty'))
-
-to_ret <- tibble()
-
-data <- game.schedule$request[1:2] %>%
-  map(~msf_get_results(version='1.2',league='nhl',season='2015-2016-regular',feed='game_playbyplay', params=list(gameid=., playtype='faceoff,goal')))
-
-test <- data %>% map(~.[["api_json"]][["gameplaybyplay"]][["plays"]][["play"]]  %>% 
-                       subset(select=c('period', 'time', 'faceoff.wonBy'))
-                     ) %>%
-  map(~split(., .$period)) %>% 
+game.data <- game.schedule$request[1:2] %>%
+  map(~msf_get_results(league='nhl',
+                       season='2015-2016-regular',
+                       feed='game_playbyplay', 
+                       params=list(gameid=., playtype='faceoff,goal')
+                      )[["api_json"]][["gameplaybyplay"]][["plays"]][["play"]]  %>% 
+      subset(select=c('period', 'time', 'faceoff.wonBy')) %>%
+      split(.$period)
+     ) %>% 
   unlist(recursive=FALSE)
+# get individual game data ------------------------------------------------
 
 
-#remove loop here!!!!!
-for(i in 1:length(test)){
-  period.calculations <- test[[i]]$time %>% 
+# process into time differences -------------------------------------------
+Time.Calculations <- function(ele){
+  period.calculations <- ele$time %>% 
     str_split(':') %>% 
     map(~.[1] %>% as.numeric() * 60 + .[2] %>% as.numeric()) %>% 
     unlist() %>% 
@@ -35,19 +43,22 @@ for(i in 1:length(test)){
     tibble() %>%
     setNames(c('playtimes'))
   
-  period.calculations$goal  <- ifelse(!is.na(test[[i]]$faceoff.wonBy),FALSE, TRUE) %>% tail(-1)
- 
-  period.calculations$period <- test[[i]]$period[1]
+  period.calculations$goal  <- ifelse(!is.na(ele$faceoff.wonBy), FALSE, TRUE) %>% tail(-1)
+  
+  period.calculations$period <- ele$period[1]
   
   period.calculations %<>% subset(playtimes != 0)
-  
-  to_ret <- rbind(to_ret, period.calculations)
 }
 
-boxplot(to_ret$playtimes)
+time.data <- map_df(game.data, Time.Calculations) %T>%
+  write.csv('C:\\Users\\Brian\\Desktop\\GradClasses\\Spring18\\nhlstats\\time_data.csv')
+# process into time differences -------------------------------------------
 
-summary(to_ret$playtimes)
 
-no.outs <- to_ret[to_ret$playtimes <= 140, ]
+# visual displays ---------------------------------------------------------
+boxplot(time.data$playtimes)
 
-ggplot(no.outs, aes(x=playtimes)) + geom_histogram(aes(fill=goal), position=position_fill(reverse=FALSE))
+ggplot(time.data) + geom_boxplot(aes(x=period, y=playtimes))
+
+ggplot(time.data, aes(x=playtimes)) + geom_histogram(aes(fill=goal), position=position_fill(reverse=FALSE))
+# visual displays ---------------------------------------------------------
