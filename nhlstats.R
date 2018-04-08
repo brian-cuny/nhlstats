440e6a9060a125d3670630ff1c7310624d1b851c
brian.weinfeld+cuny@gmail.com <brian.weinfeld+cuny@gmail.com>
2018-04-08 19:16:29 -0400

    some nonsense statistics work

--- nhlstats.R
+++ nhlstats.R
@@ -1,10 +1,22 @@
 library(tidyverse)
 library(magrittr)
 library(mysportsfeedsR)
-authenticate_v1_x('njpsy','asdfasdf')
+library(httr)
+library(jsonlite)
+library(httr)
+library(RCurl)
+library(XML)
 
+test <- GET('https://api.mysportsfeeds.com/v1.2/pull/nhl/2015-2016-regular/full_game_schedule.json', authenticate('njpsy', 'asdfasdf'), add_headers('Content-Type'='application/json', 'Accept-Encoding'='gzip')) %>%
+  content(as='text') %>%
+  fromJSON(flatten=TRUE) %>%
+  .[[1]] %>%
+  .$gameentry %>%
+  as.tibble()
 
 # get NHL schedule --------------------------------------------------------
+authenticate_v1_x('njpsy','asdfasdf')
+
 game.schedule <- msf_get_results(league='nhl', 
                                  season='2015-2016-regular', 
                                  feed='full_game_schedule')[["api_json"]][["fullgameschedule"]][["gameentry"]] %>%
@@ -24,9 +36,9 @@ TimeToSeconds <- function(ele){
     unlist()
 }
 
-game.schedule <- read.csv('C:\\Users\\Brian\\Desktop\\GradClasses\\Spring18\\nhlstats\\schedule.csv')
+game.schedule <- read.csv('C:\\Users\\Brian\\Desktop\\GradClasses\\Spring18\\nhlstats\\schedule.csv', stringsAsFactors=FALSE)
 
-game.data <- game.schedule$request[1:200] %>%
+game.data <- game.schedule$request %>%
   map_dfr(~msf_get_results(league='nhl',
                        season='2015-2016-regular',
                        feed='game_playbyplay', 
@@ -46,7 +58,7 @@ play.lengths <- c(0, diff(game.data$seconds))
 time.diffs <- game.data %>%
   cbind(play.lengths) %>%
   subset(play.lengths > 0, select=c('id', 'period', 'play.lengths', 'result')) %T>%
-  write.csv('C:\\Users\\Brian\\Desktop\\GradClasses\\Spring18\\nhlstats\\time_data.csv')
+  write.csv('C:\\Users\\Brian\\Desktop\\GradClasses\\Spring18\\nhlstats\\time_data.csv', append='a')
 # process into time differences -------------------------------------------
 
 
@@ -58,3 +70,48 @@ summary(time.diffs$play.lengths)
 
 ggplot(time.diffs %>% subset(play.lengths <= 175), aes(x=play.lengths)) + geom_histogram(aes(fill=result), position=position_fill(reverse=TRUE), binwidth=5)
 # visual displays ---------------------------------------------------------
+
+
+
+ppg <- 48/82
+
+ppg <- 48/81
+
+in.playoffs <- tibble(success=rbinom(n=10000, size=82, prob=ppg), size=82)
+
+in.playoffs %>%
+  filter(success >= 48) %>%
+  count()
+
+ggplot(data=data.frame(x=c(0,82)), aes(x)) +
+  stat_function(fun=dnorm, args=list(mean=82*.5, sd=sqrt(82*0.5*0.5)), color='red') +
+  stat_function(fun=dnorm, args=list(mean=10 + 72*.5, sd=sqrt(82*0.5*0.5)), color='blue')
+
+pbinom(q=47, size=82, prob=0.58, lower.tail=FALSE)
+pbinom(q=0.5, size=3, prob=0.5825, lower.tail=FALSE)
+
+
+devils.data.html <- getURL('https://www.hockey-reference.com/teams/NJD/2018_games.html') %>% 
+  htmlParse()
+
+devils.data <- devils.data.html %>%
+  xpathSApply('//*[@id="games"]/tbody/tr//td[7]', xmlValue) 
+
+result.data <- devils.data.html %>%
+  xpathSApply('//*[@id="games"]/tbody/tr//td[8]', xmlValue) 
+
+devils.t <- tibble(game=0:82, result=c('', devils.data), mod=c('', result.data)) %>%
+  mutate(points = ifelse(result == 'W', 2, ifelse(result == 'L' & (mod == 'OT' | mod == 'SO'), 1, 0))) %>%
+  mutate(remaining = 95-cumsum(points)) %>%
+  mutate(games.left = 82-game) %>%
+  mutate(remaining = ifelse(remaining > 0, remaining, -1)) %>%
+  mutate(prob=pbinom(q=remaining/2, size=games.left, prob=0.58, lower.tail=FALSE)) %>%
+  #mutate(prob=pnorm(remaining/(82-game), mean=48/82, sd=48/82/3, lower.tail=FALSE)) %>%
+  mutate(prob=ifelse(is.na(prob), 1, prob))
+
+
+devils.t %>%
+  ggplot(aes(game, prob)) +
+  geom_point()
+
+#http://www.sportsclubstats.com/NHL.html
