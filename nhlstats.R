57ae74e2afa6692dc7c5fa88779753b8def157ca
brian.weinfeld+cuny@gmail.com <brian.weinfeld+cuny@gmail.com>
2018-04-09 20:50:22 -0400

    updated graphs

--- nhlstats.R
+++ nhlstats.R
@@ -110,17 +110,15 @@ Team.Data.Collect <- function(x){
     mutate(points = ifelse(V1 == 'W', 2, ifelse(V1 == 'L' & (V2 %in% c('OT', 'SO')), 1, 0)),
            total.points = cumsum(points),
            resid = total.points - game*win.rate, 
-           team = x
+           team = x,
+           last = last(game[resid > 0 & lag(resid) < 0])
     )
 }
-
 
 points.data <- team.ids$team %>%
   map_df(~Team.Data.Collect(.)) %>%
   select(-V1, -V2) %>%
   select(team, everything())
-
-
 
 points.data %>%
   ggplot(aes(game, resid)) +
@@ -138,14 +136,16 @@ summary.points.data <- points.data %>%
   summarise(min = min(resid),
             max = max(resid),
             total.points = max(total.points), 
-            end.position = last(resid)
+            end.position = last(resid),
+            last = last(game[(resid >= 0 & lag(resid) <= 0) | (resid <= 0 & lag(resid) >= 0)])
             ) %>%
   arrange(desc(total.points))
 
-ggplot(summary.points.data, aes(color=total.points > 95)) +
-  geom_point(aes(reorder(team, total.points), end.position), size=2, show.legend=FALSE) +
+ggplot(summary.points.data, aes(reorder(team, total.points), end.position, color=total.points > 95)) +
+  geom_point(size=2, show.legend=FALSE) +
   geom_segment(aes(x=team, xend=team, y=min, yend=max), show.legend=FALSE) +
   geom_hline(yintercept=0, color='black') +
+  geom_text(aes(y=-35, label=last), show.legend=FALSE) +
   scale_y_continuous(limits=c(-35, 25), breaks=seq(-35, 25, 5)) +
   coord_flip() +
   labs(y='Net Points',
